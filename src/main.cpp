f5d7e6bc483331dd5a3b075ea332aca359cf6419
mduramaz <mehmetduramaz@gmail.com>
2022-05-02 09:24:36 +0300

    image.h added getpixelR-G-B functions

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,11 @@ int main(int argc, char *argv[])
    mycv::Image image("C:\\Users\\mehmet\\Desktop\\myCvLibWs\\chess.bmp");
    image.write("C:\\Users\\mehmet\\Desktop\\myCvLibWs\\chess3.bmp");
    
+   
+     /*  for(int i=0; i<10000; i++){
+    mycv::pixel* pixel=image.getpixel(i);
+    printf("\n %d \n",(int)pixel[1]);
+    }*/
+    
    return 0;
 }
